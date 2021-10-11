5641780822339656970e7f118afeda5823b5f513
pedrohemmel <phenrique_h2016@hotmail.com>
2021-10-11 15:39:08 -0300

    nova atualização (seção de projetos)

--- js/script.js
+++ js/script.js
@@ -11,13 +11,8 @@ function mudaIdioma() {
     let entreEmContatoHeader = document.getElementById('entreEmContatoHeader').innerHTML = 'Contact me';
     let apresentacaoText1 = document.getElementById('apresentacaoText1').innerHTML = 'Hi, my name is';
     let apresentacaoText3 = document.getElementById('apresentacaoText3').innerHTML = 'Front-end Developer | Database Analyst';
-    let meusProjetosText = document.getElementById('meusProjetosText').innerHTML = 'My Projects';
-    let projeto1 = document.getElementById('projeto1').innerHTML = 'Snake Game';
-    let cat1 = document.getElementById('cat1').innerHTML = 'Game';
-    let projeto2 = document.getElementById('projeto2').innerHTML = 'Tic-tac-toe';
-    let cat2 = document.getElementById('cat2').innerHTML = 'Game';
-    let projeto3 = document.getElementById('projeto3').innerHTML = 'Servers';
-    let cat3 = document.getElementById('cat3').innerHTML = 'Article';
+    let meusProjetosTitulo = document.getElementById('meusProjetosTitulo').innerHTML = 'Projects';
+    let meusProjetosTexto = document.getElementById('meusProjetosTexto').innerHTML = 'One of the better ways to learn is practicing, and this is what i get from my projects, and i also take the opportunity to put into my academic portfolio.';
     let entreEmContatoFooter = document.getElementById('entreEmContatoFooter').innerHTML = 'Contact me';
     let direitos = document.getElementById('direitos').innerHTML = 'All rights reserved';
     let alocadorLinguagem = document.getElementById('alocadorLinguagem').innerHTML = 'Português - PT-BR';
